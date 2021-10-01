ce909666e08518380fe076a5d340a4ce88c123cd
renata monteiro <r.cardoso.monteiro1@gmail.com>
2021-10-01 15:40:08 -0300

    novo

--- python_116_sobrecargar.py
+++ python_116_sobrecargar.py
@@ -1,2 +1,48 @@
 #SOBRECARGA DE OPERADORES
 
+class Retangulo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_area(self):
+        return self.x * self.y
+
+    def __repr__(self):
+        return f"<class 'Retangulo({self.x}, {self.y})'>"
+
+    def __add__(self, other): #self é a primeira instancia - other é o segundo objeto
+        novo_x = self.x + other.x
+        novo_y = self.y + other.y
+        return Retangulo(novo_x, novo_y)
+
+    def __lt__(self, other): #menor que
+        a1 = self.get_area()
+        a2 = other.get_area()
+
+        if a1 < a2:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other): #maior que
+        a1 = self.get_area()
+        a2 = other.get_area()
+
+        if a1 > a2:
+            return True
+        else:
+            return False
+
+    def __eq__(self, other):  # maior que
+        if self.x == other.x and self.y == other.y:
+            return True
+        else:
+            return False
+
+r1 = Retangulo(10, 20)
+r2 = Retangulo(10, 20)
+r3 = r1 + r2
+
+print(r3 < r1)
+print(r1 == r3)
---
+++ python_117.py
@@ -0,0 +1,44 @@
+#METODOS MAGICOS
+
+class A:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, '_jaexiste'):
+            cls._jaexiste = object.__new__(cls)
+        return cls._jaexiste
+        # def haha(*args, **kwargs):
+        #     print("Fala Oi")
+        # cls.haha = haha
+
+        #return object.__new__(cls)
+
+    def __call__(self, *args, **kwargs): #faz a classe se comportar como uma funcao
+        resultado = 1
+        for i in args:
+            resultado *= i
+        return resultado
+
+        # print(args)
+        # print(kwargs)
+
+    def __init__(self):
+        print('EU SOU O INIT')
+
+    def __setattr__(self, key, value):
+        if key == 'nome':
+            self.__dict__[key] = 'você nao pode fazer isso'
+        else:
+            self.__dict__[key] = value
+
+a = A()
+variavel = a(1,2,4,5,6,7,8,9,10)
+print(variavel)
+a.nome = 'Luiz Otavio'
+print(a.nome)
+a.qualquer = 225
+print(a.nome, a.qualquer)
+# a(1,2,3,4,5, nome='Luiz')
+#a.haha()
+# b = A()
+# c = A()
+
+#print(id(a), id(b), id(c)) #todas sao o mesmo objeto
